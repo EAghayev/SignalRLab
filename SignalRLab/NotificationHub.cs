0f43c7374d59040433ccd81d3fb3c49f490feb3f
Elchin Aghayev <elchinia@code.edu.az>
2019-02-28 00:46:16 +0400

    SignalR Lab

---
+++ SignalRLab/NotificationHub.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRLab
+{
+    public class NotificationHub : Hub
+    {
+        static Dictionary<string, string> Users = new Dictionary<string, string>();
+
+        public void Hello()
+        {
+            Clients.Others.hi();
+        }
+
+        public override Task OnConnected()
+        {
+            string FullName= this.Context.QueryString["fullName"];
+
+            return base.OnConnected();
+        }
+
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string FullName = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            Clients.Others.ShowDiss(FullName);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public void Enter(string Name,string Surname)
+        {
+            string FullName = Name + " " + Surname;
+            Clients.Others.ShowUsers(Name, Surname);
+            Users.Add(Context.ConnectionId, FullName);
+        }
+    }
+}
